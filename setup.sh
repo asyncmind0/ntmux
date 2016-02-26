ceb30cb8a3edce4c53a29f1f14bcb6d187fb6fcf
Steven Joseph <steven@stevenjoseph.in>
2016-02-26 14:09:17 +1100

    create .local/bin if not exist

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 pip install --user --upgrade -r requirements.txt
 mkdir ~/.local/bin
+mkdir -p ~/.local/bin
 ln -sf ~/.tmux/autossh.py ~/.local/bin/
 ln -sf ~/.tmux/tmux.py ~/.local/bin/
 ln -sf ~/.tmux/inner.conf ~/.tmux.conf
