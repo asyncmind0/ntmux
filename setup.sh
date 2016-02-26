dc17488ff60521871a6e307175a54d7bcebab394
Steven Joseph <steven@stevenjoseph.in>
2016-02-26 14:10:38 +1100

    fix mkdir -p

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 pip install --user --upgrade -r requirements.txt
-mkdir ~/.local/bin
-mkdir -p ~/.local/bin
+mkdir -p ~/.local/bin/
 ln -sf ~/.tmux/autossh.py ~/.local/bin/
 ln -sf ~/.tmux/tmux.py ~/.local/bin/
 ln -sf ~/.tmux/inner.conf ~/.tmux.conf
