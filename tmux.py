910bcd3d6402658cda753aec9cf83e5356aad417
Steven Joseph <steven@stevenjoseph.in>
2018-01-11 23:35:01 +1100

    Refactor config into yaml

--- tmux.py
+++ tmux.py
@@ -6,13 +6,12 @@ tmux.py
 Usage: tmux.py <server> <session> [options]
 
 Options:
-    --base-index=BASE_INDEX
-    --window-name=WINDOW_NAME
-    -c
-    -d      Start detached
-    -k      kill server
-    -r      remote server
-    -l      list sessions
+  --window-name=WINDOW_NAME
+  -c --config=STRING        window configuration file [default: ~/.tmux/configs/default.yml]
+  -d                        Start detached
+  -k                        kill server
+  -r                        remote server
+  -l                        list sessions
 
 
 """
@@ -22,125 +21,39 @@ from os.path import expanduser
 from tmuxp import Server, WorkspaceBuilder, exc
 import platform
 import logging
+import yaml
 
 logging.basicConfig(level=logging.INFO)
 
 
 formatted_hostname = platform.node().split('.')[0].lower()
-cmd = ("python %s inner {name} "
-       "--base-index={base_index}") % __file__
+default_inner_cmd = ("python %s inner {name} ") % __file__
 
 
-def window_config(name, base_index=0):
-    args = dict(name=name, base_index=base_index)
+def window_config(name, window_config):
     return {
         'window_name': name,
         'panes': [
             {
-                'shell_command': [
-                    cmd.format(**args),
-                ]
+                'shell_command': [window_config.get('shell_command', default_inner_cmd.format(
+                    name=name))],
             },
         ],
     }
 
 
-def session_config(name, config_map=None, windows=None, base_index=0):
+def session_config(name, config_map=None, windows=None):
     session_map = {
         'session_name': name,
         'start_directory': '~',
         'windows': windows or [],
         'options': {
-            'base-index': base_index
+            'base-index': 0
         },
     }
     if config_map is not None:
         config_map[name] = session_map
     return session_map
-
-
-wc = window_config
-sc = session_config
-
-series9 = {}
-sc('left', series9, [
-    wc('python'),
-    wc('devops'),
-    wc('js'),
-    wc('android'),
-    wc('ios'),
-    wc('home'),
-])
-
-sc('right', series9, [
-    wc('python_dev'),
-    wc('devops_dev'),
-    wc('js_dev'),
-    wc('android_dev'),
-    wc('ios_dev'),
-    wc('home_dev'),
-])
-
-sc('xplan', series9)
-sc('conf', series9)
-sc('monitor', config_map=series9)
-sc('media', config_map=series9)
-sc('tleft', series9, [wc('txplan')])
-
-acer = {}
-sc('acer', acer)
-webpi = {}
-sc('webpi', webpi)
-
-xbmc = {}
-sc('raspbmc', config_map=xbmc)
-
-iress = {
-    'bg': 'red',
-    'fg': 'white'
-}
-
-sc('left', config_map=iress, windows=[
-    wc('xplan'),
-    wc('manup'),
-    wc('conf'),
-], base_index=0)
-
-sc('right', config_map=iress, windows=[
-    wc('xplandev'),
-    wc('manupdev'),
-    wc('oscscope'),
-    wc('misc'),
-], base_index=0)
-
-sc('comm', config_map=iress, windows=[
-    wc('jabber'),
-    wc('email'),
-], base_index=0)
-
-sc('tleft', config_map=iress, windows=[
-    wc('txplan')], base_index=0)
-sc('xplan', config_map=iress)
-sc('manup', config_map=iress)
-sc('xplandev', config_map=iress)
-sc('manupdev', config_map=iress)
-sc('conf', config_map=iress)
-sc('jabber', config_map=iress)
-sc('misc', config_map=iress)
-sc('process', config_map=iress)
-
-iress2 = dict(iress)
-iress2['bg'] = 'blue'
-
-config_map = {
-    'steven-series9': series9,
-    'acer': acer,
-    'sydsjoseph-pc1': iress,
-    'au02-sjosephpc2': iress2,
-    'webpi': webpi,
-    'xbmc': xbmc,
-    'raspbmc': xbmc,
-}
 
 
 def get_status_line(host_config, remote):
@@ -164,14 +77,22 @@ if __name__ == "__main__":
     print(args)
     server_name = args['<server>']
     session_name = args['<session>']
-    base_index = int(args['--base-index'] or 0)
-    shell_command = args['-c'] or ''
     remote = args['-r']  # or os.environ.get('SSH_TTY')
     kill = args['-k']
     os.environ['TERM'] = 'xterm-256color'
 
-    host_config = config_map.get(formatted_hostname, {})
-    session_config = host_config.get(session_name, sc(session_name))
+    host_config = {}
+    for outer, inners in yaml.load(open(expanduser(args['--config'])))['windows'].items():
+        session_config(
+            outer,
+            host_config,
+            [
+                window_config(name, config)
+                for name, config in inners['windows'].items()
+            ]
+        )
+
+    session_config = host_config.get(session_name, session_config(session_name))
     windows = session_config['windows']
     for i in range(10-len(windows)):
         session_config['windows'].append({
@@ -193,7 +114,7 @@ if __name__ == "__main__":
                 {'session_name': session_name})
             session.kill_session()
         except Exception as e:
-            print(e)
+            logging.exception("Error killing session")
             pass
         exit(0)
     if args.get('-l'):
@@ -203,12 +124,12 @@ if __name__ == "__main__":
         session = server.new_session(
             session_name=session_name)
     except exc.TmuxSessionExists:
-        print("Session exists")
+        logging.debug("Session exists")
         pass
     try:
         builder.build(session)
     except exc.TmuxSessionExists:
-        print("builder session exists")
+        logging.debug("builder session exists")
         session = session or builder.session or server.findWhere(
             {'session_name': session_name})
 
@@ -235,14 +156,10 @@ if __name__ == "__main__":
     if not args['-d']:
         server.cmd("detach-client", "-s", session_name)
 
-    session.set_option('base-index', base_index)
 
     status_right, status_left = get_status_line(host_config, remote)
-    print(status_right)
-    print(status_left)
     for session in server.list_sessions():
         try:
-            print(session.get('session_name'))
             session.set_option("status-right", status_right)
             session.set_option("status-left", status_left)
             session.set_environment('SSH_AUTH_SOCK', os.environ.get('SSH_AUTH_SOCK'))
@@ -256,6 +173,6 @@ if __name__ == "__main__":
             logging.exception("error setting up session")
 
     if not args['-d']:
-        if 'TMUX' in os.environ:
+        if 'TMUX' in os.environ and server_name == 'outer':
             os.system('tmux rename-window %s' % session_name)
         server.attach_session(session_name)
