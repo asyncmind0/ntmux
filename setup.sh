77586b0eef600efd8835d23998dd905da4886c9a
Steven Joseph <steven@stevenjoseph.in>
2018-07-24 08:40:28 +1000

    Add switches to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
-pip install --upgrade -r requirements.txt
-mkdir -p ~/.local/bin/
-ln -sf ~/.tmux/autossh.py ~/.local/bin/
-ln -sf ~/.tmux/tmux.py ~/.local/bin/
-ln -sf ~/.tmux/inner.conf ~/.tmux.conf
+#!/bin/sh -x
+BASE_DIR=/usr/local/etc/zsh
+if [ ! -d $BASE_DIR ]; then
+    BASE_DIR=$HOME/.zsh
+fi
+if [ -z "$NO_PIP_INSTALL" ]; then
+pip install -U --user -r $BASE_DIR/requirements.txt
+fi
+mkdir -p $HOME/.local/bin/
+ln -sf $HOME/.tmux/autossh.py $HOME/.local/bin/
+ln -sf $HOME/.tmux/tmux.py $HOME/.local/bin/
+ln -sf $HOME/.tmux/inner.conf $HOME/.tmux.conf
