488b58fce8ad3c207bbeaa365e1fbe0a0b0bcc97
Steven Joseph <steven@stevenjoseph.in>
2018-08-16 11:10:03 +1000

    Setup for binary distribution

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ if [ -z "$NO_PIP_INSTALL" ]; then
     pip install -U --user -r $BASE_DIR/requirements.txt
 fi
 mkdir -p $HOME/.local/bin/
+wget -o tmuxpy.zip "https://gitlab.com/api/v4/projects/7957517/jobs/artifacts/master/download?job=docker"
 ln -sf $HOME/.tmux/autossh.py $HOME/.local/bin/
 ln -sf $HOME/.tmux/tmux.py $HOME/.local/bin/
 ln -sf $HOME/.tmux/tmuxpy $HOME/.local/bin/
