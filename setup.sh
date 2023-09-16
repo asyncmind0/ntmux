8993077b4c5ccd20f99aa7325c012d842781184a
Steven Joseph <steven@stevenjoseph.in>
2023-09-16 20:40:48 +1000

    Fix install requirements break shit

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [ ! -d $BASE_DIR ]; then
     BASE_DIR=$HOME/.tmux/
 fi
 if [ -z "$NO_PIP_INSTALL" ]; then
-    pip install -U --user -r $BASE_DIR/requirements.txt
+    pip install -U --user --break-system-packages -r $BASE_DIR/requirements.txt
 fi
 mkdir -p $HOME/.local/bin/
 wget -o tmuxpy.zip "https://gitlab.com/api/v4/projects/7957517/jobs/artifacts/master/download?job=docker"
